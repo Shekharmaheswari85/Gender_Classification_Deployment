76487b2f961ef8c909cc7b97d396299102eba5fe
shekharmaheswari <shekharmaheswari85@gmail.com>
2020-09-16 17:50:00 +0530

    Final project

---
+++ App/views.py
@@ -0,0 +1,36 @@
+from flask import render_template,request
+from flask import url_for,redirect
+import os
+from PIL import Image
+from utility import utils 
+
+UPLOAD_FOLDER='static/upload'
+
+def base():
+    return render_template('base.html')
+
+def index():
+    return render_template('index.html')
+
+def faceapp():
+    return render_template('faceapp.html')
+
+def getwidth(path):
+    img=Image.open(path)
+    size = img.size # width and height
+    aspect = size[0]/size[1] # width/height
+    w = 300 * aspect
+    return int(w)
+
+def gender():
+    if request.method=='POST':
+        f= request.files['image']
+        filename=f.filename
+        path=os.path.join(UPLOAD_FOLDER,filename)
+        f.save(path)
+        # preprocessing
+        w=getwidth(path)
+        # prediction (pass to pipeline model)
+        text=utils.pipeline_model(path,filename,color='bgr')
+        return render_template('gender.html',fileupload=True,img_name=filename,w=w,text=text)    
+    return render_template('gender.html',fileupload=False,img_name="appicon.jpeg",w=300,text=None)
---
+++ utility/utils.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+import sklearn
+import cv2 as cv
+from PIL import Image
+
+#loading model
+haar=cv.CascadeClassifier('./model/haarcascade_frontalface_default.xml')
+#Pickle files
+mean=pickle.load(open('./model/mean_preprocess.pickle','rb'))
+model_svm=pickle.load(open('./model/model_svm.pickle','rb'))
+model_pca=pickle.load(open('./model/pca_50.pickle','rb'))
+print("Model loaded successfully")
+
+#settings
+gender_pre=['Male','Female']
+font=cv.FONT_HERSHEY_SIMPLEX
+
+def pipeline_model(path,filename,color='rgb'):
+    # step - 1: read image in cv
+    img=cv.imread(path)
+    # step - 2 : convert to gray scale
+    if color=='bgr':
+        gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    else:
+        gray=cv.cvtColor(img,cv.COLOR_RGB2GRAY)
+    # step - 3: Crop the image using haar cascade classifier
+    faces=haar.detectMultiScale(gray,1.5,3)
+    for x,y,w,h in faces:
+        cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3) #drawing contour or rectangle
+        roi=gray[y:y+h,x:x+w] # crop image
+        # step - 4: normalization (0-1)
+        roi=roi/255.0
+        # step - 5: resize image (100,100)
+        if roi.shape[1]>100:
+            roi_resize=cv.resize(roi,(100,100),cv.INTER_AREA)
+        else:
+            roi_resize=cv.resize(roi,(100,100),cv.INTER_CUBIC)
+        # step - 6: Falttening (1x10000)
+        roi_reshape=roi_resize.reshape(1,10000) #1 , -1
+        # step - 7: substract with mean
+        roi_mean=roi_reshape-mean
+        #step - 8: get eigen image
+        eigen_image=model_pca.transform(roi_mean)
+        #step - 9: pass to ml Model(svm)
+        results=model_svm.predict_proba(eigen_image)[0]
+        #step - 10: 
+        predict= results.argmax() # 0 or 1
+        score=results[predict ]
+        #step - 11:
+        text = "%s : %0.2f"%(gender_pre[predict],score)
+        print(text)
+        cv.putText(img,text,(x,y),font,1,(255,255,0),2)
+    cv.imwrite('./static/predict/{}'.format(filename),img)
+    return text
+ 
